c3e471be97fab32e285a74c3ed0f0612fec5e0bf
McConnell <ik.ugwuanyi@gmail.com>
2023-10-18 03:28:20 +0100

    added a user table and set todo relationship with user

--- todo_app/db/models.py
+++ todo_app/db/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from sqlalchemy import Boolean, String, DateTime, Column, Integer
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Boolean, String, DateTime, Column, Integer, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
@@ -9,9 +9,23 @@ class Todo(Base):
     __tablename__ = "todos"
 
     id = Column(Integer, primary_key=True, index=True)
-    owner = Column(String)
+    owner = Column(Integer, ForeignKey("users.id"), nullable=False)
     title = Column(String)
     description = Column(String)
     completed = Column(Boolean, default=False)
     completed_at = Column(DateTime, nullable=True)
     created_at = Column(DateTime, default=datetime.now())
+
+    todo_owner = relationship("User", back_populates="todos")
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String, nullable=True)
+    last_name = Column(String, nullable=True)
+    username = Column(String(15), nullable=False, unique=True)
+    created_at = Column(DateTime, default=datetime.now())
+
+    todos = relationship("Todo", back_populates="todo_owner")
