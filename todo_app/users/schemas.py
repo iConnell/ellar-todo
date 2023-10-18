d43f55937db46d7645053a0d3e902e0f484e259b
McConnell <ik.ugwuanyi@gmail.com>
2023-10-18 05:23:44 +0100

    added user module and added user schema

---
+++ todo_app/users/schemas.py
@@ -0,0 +1,26 @@
+"""
+Define Serializers/DTOs
+Example:
+
+class ASampleDTO(Serializer):
+    name: str
+    age: t.Optional[int] = None
+
+for dataclasses, Inherit from DataclassSerializer
+
+@dataclass
+class ASampleDTO(DataclassSerializer):
+    name: str
+    age: t.Optional[int] = None
+"""
+from ellar.common import DataclassSerializer, Serializer
+
+
+class UserSerializer(Serializer):
+    first_name: str
+    last_name: str
+    username: str
+
+
+class RetrieveUserSerilizer(UserSerializer):
+    pk: str
